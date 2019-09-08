5535e22f01739d725437f87af2e610dbc9f8690f
raythurman2386 <raymondthurman5@gmail.com>
2019-09-08 00:07:01 -0400

    added a nav i was playing with for no reason

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// Components
+import Navbar from './components/navbar/Navbar'
+import GlobalStyle from './styles/Global'
 
 function App() {
+  // Hook for just the navbar
+  const [navBarOpen, setNavBarOpen] = useState(false)
+
+  // // Handler for the nav bar
+  const handleNavbar = () => {
+    setNavBarOpen(!navBarOpen)
+  }
+
   return (
     <div className='App'>
+      <Navbar navbarState={navBarOpen} handleNavbar={handleNavbar} />
       <header className='App-header'>
         <h1>Team Builder</h1>
+        <GlobalStyle />
       </header>
     </div>
   )
