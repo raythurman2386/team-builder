9201b8ad578d0364259d44c60823ab08f678c054
raythurman2386 <raymondthurman5@gmail.com>
2019-09-10 19:54:16 -0400

    added required to the form inputs

--- team-builder/src/components/form/Form.js
+++ team-builder/src/components/form/Form.js
@@ -1,74 +1,56 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const Form = ({ teamList, setTeamList, history }) => {
   // make an initial team to reset the form to
-  const initialForm = { name: '', email: '', role: '' }
+  const initialForm = { name: '', email: '', role: '' };
   // Form hook
-  const [form, setForm] = useState(initialForm)
+  const [ form, setForm ] = useState(initialForm);
 
   // Initial handleChange for the form
-  const handleChange = event => {
-    const { name, value } = event.target
+  const handleChange = (event) => {
+    const { name, value } = event.target;
     setForm({
       ...form,
-      [name]: value,
-    })
-  }
+      [name] : value,
+    });
+  };
 
   // Handle submit
-  const handleSubmit = event => {
-    event.preventDefault()
-    setTeamList([...teamList, form])
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTeamList([ ...teamList, form ]);
     // route to the homepage on submit
-    history.push("/")
+    history.push('/');
     // resets form as soon as it's submit
-    resetForm()
-  }
+    resetForm();
+  };
 
   // function to reset the form
   const resetForm = () => {
-    setForm(initialForm)
-  }
+    setForm(initialForm);
+  };
 
   return (
     <div>
       <FormWrapper onSubmit={handleSubmit}>
-        <Input
-          type='text'
-          value={form.name}
-          onChange={handleChange}
-          name='name'
-          placeholder='Name'
-        />
-        <Input
-          type='text'
-          value={form.email}
-          onChange={handleChange}
-          name='email'
-          placeholder='Email'
-        />
-        <Input
-          type='text'
-          value={form.role}
-          onChange={handleChange}
-          name='role'
-          placeholder='Role'
-        />
+        <Input type='text' value={form.name} onChange={handleChange} name='name' placeholder='Name' required />
+        <Input type='text' value={form.email} onChange={handleChange} name='email' placeholder='Email' required />
+        <Input type='text' value={form.role} onChange={handleChange} name='role' placeholder='Role' required />
         <ButtonWrapper>Submit</ButtonWrapper>
       </FormWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
 
 const FormWrapper = styled.form`
   display: flex;
   flex-direction: column;
   margin: auto;
   align-items: center;
-`
+`;
 
 const Input = styled.input`
   margin: 10px 0;
@@ -77,9 +59,8 @@ const Input = styled.input`
   // border-radius: 8px;
   padding: 5px 10px;
   width: 60%;
-  box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12),
-    0 2px 4px rgba(0, 0, 0, 0.24);
-`
+  box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.24);
+`;
 
 const ButtonWrapper = styled.button`
   cursor: pointer;
@@ -87,11 +68,9 @@ const ButtonWrapper = styled.button`
   padding: 8px 14px;
   background-color: lightskyblue;
   border: none;
-  box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12),
-    0 2px 4px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 4px rgba(0, 0, 0, 0.24);
 
   &:hover {
-    box-shadow: 0 -1px 10px #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12),
-      0 2px 10px rgba(0, 0, 0, 0.24);
+    box-shadow: 0 -1px 10px #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12), 0 2px 10px rgba(0, 0, 0, 0.24);
   }
-`
+`;
