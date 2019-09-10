bd56ef4cf8dfbf5705473e74d85c627b781698dc
raythurman2386 <raymondthurman5@gmail.com>
2019-09-09 21:13:08 -0400

    minor card styling

--- team-builder/src/components/team/TeamMember.js
+++ team-builder/src/components/team/TeamMember.js
@@ -3,14 +3,23 @@ import styled from 'styled-components'
 
 const TeamMember = ({ member }) => {
   return (
-    <div>
+    <TeamMemberCard>
       <h1>{member.name}</h1>
       <h2>{member.email}</h2>
       <h3>{member.role}</h3>
-    </div>
+    </TeamMemberCard>
   )
 }
 
 export default TeamMember
 
-const TeamMemberCard = styled.div``
+const TeamMemberCard = styled.div`
+  width: 100%;
+  min-height: 250px;
+  background-color: #f2f2f2;
+  padding: 1.5rem;
+  -webkit-box-shadow: 0 10px 6px -6px #777;
+  -moz-box-shadow: 0 10px 6px -6px #777;
+  box-shadow: 0 10px 6px -6px #777;
+  margin: 2rem auto;
+`
