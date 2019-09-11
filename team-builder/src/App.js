7ddc7d814ab12f8f616d307ec6d382de3f39e347
raythurman2386 <raymondthurman5@gmail.com>
2019-09-10 22:34:41 -0400

    couple bugs but it's done for now

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Route, Switch } from 'react-router-dom';
 // Components
