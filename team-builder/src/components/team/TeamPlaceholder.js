7732e595eaf77d41014a6c967ef51b81e618e6ca
raythurman2386 <raymondthurman5@gmail.com>
2019-09-13 19:33:05 -0400

    added svg image

--- team-builder/src/components/team/TeamPlaceholder.js
+++ team-builder/src/components/team/TeamPlaceholder.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import image from '../../assets/workers-team.svg'
+
 const TeamPlaceholder = () => {
   return (
     <div>
+      <img src={image} alt='One Team' />
       <h1>Oh dear! It appears you don't have a team!</h1>
       <p>How about adding your team members?</p>
     </div>
