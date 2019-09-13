74f5111fdd28020c3bd6db29b16f0e1e73c79b82
raythurman2386 <raymondthurman5@gmail.com>
2019-09-12 20:35:20 -0400

    refactoring to formik

--- team-builder/src/components/form/Form.js
+++ team-builder/src/components/form/Form.js
@@ -1,71 +1,32 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import { Form, Field, withFormik } from 'formik'
 import * as Yup from 'yup'
 
 const TeamForm = (props, { errors, touched, status }) => {
-  let id = 1 || props.teamList.length + 1;
-  // make an initial team to reset the form to
-  let initialForm;
-  if (!props.isEditable) {
-    initialForm = { id: id, name: '', email: '', role: '' };
-  } else {
-    const member = props.teamList.find((teamMember) => {
-      return teamMember.id === Number(props.match.params.id);
-    });
-    initialForm = {
-      id: member.id,
-      name: member.name,
-      email: member.email,
-      role: member.role,
-    };
-  }
+  const [formValues, setFormValues] = useState({})
 
-  // Handle submit
-  //   if (props.teamList === []) {
-  //     props.setTeamList([form]);
-  //   } else {
-  //     props.setTeamList([...props.teamList, form]);
-  //   }
-  //   if (props.isEditable) {
-  //     props.setIsEditable(!props.isEditable);
-  //     // filter then push
-  //     let filteredList = props.teamList.filter((item) => item.id !== form.id);
-  //     // Set the edited form to the new list
-  //     props.setTeamList([...filteredList, form]);
-  //   }
-  //   // route to the homepage on submit
-  //   props.history.push('/');
-  //   // resets form as soon as it's submit
-  //   resetForm();
-  // };
+  // set the values and and id
+  useEffect(() => {
+    setFormValues({ ...status, id: Date.now() })
+  }, [status])
+
+  // set the values to state from app
+  useEffect(() => {
+    props.setTeamList([...props.teamList, formValues])
+  }, [formValues])
 
   return (
     <div>
       <FormWrapper>
-        <Input
-          type='text'
-          name='name'
-          placeholder='Name'
-          required
-        />
-        <Input
-          type='email'
-          name='email'
-          placeholder='Email'
-          required
-        />
-        <Input
-          type='text'
-          name='role'
-          placeholder='Role'
-          required
-        />
+        <Input type='text' name='name' placeholder='Name' required />
+        <Input type='email' name='email' placeholder='Email' required />
+        <Input type='text' name='role' placeholder='Role' required />
         <ButtonWrapper>Submit</ButtonWrapper>
       </FormWrapper>
     </div>
-  );
-};
+  )
+}
 
 export default withFormik({
   mapPropsToValues: ({ name, email, role }) => {
@@ -79,7 +40,9 @@ export default withFormik({
   // Validation
   validationSchema: Yup.object().shape({
     name: Yup.string().required(),
-    email: Yup.string().email().required(),
+    email: Yup.string()
+      .email()
+      .required(),
     role: Yup.string().required(),
   }),
 
@@ -87,15 +50,15 @@ export default withFormik({
   handleSubmit(values, { setStatus }) {
     console.log(values)
     setStatus(values)
-  }
-})(TeamForm);
+  },
+})(TeamForm)
 
 const FormWrapper = styled(Form)`
   display: flex;
   flex-direction: column;
   margin: auto;
   align-items: center;
-`;
+`
 
 const Input = styled(Field)`
   margin: 10px 0;
@@ -106,7 +69,7 @@ const Input = styled(Field)`
   width: 60%;
   box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12),
     0 2px 4px rgba(0, 0, 0, 0.24);
-`;
+`
 
 const ButtonWrapper = styled.button`
   cursor: pointer;
@@ -121,4 +84,4 @@ const ButtonWrapper = styled.button`
     box-shadow: 0 -1px 10px #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12),
       0 2px 10px rgba(0, 0, 0, 0.24);
   }
-`;
+`
