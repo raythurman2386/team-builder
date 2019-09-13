15fac826f426e7b46c2dd2a9d4c329b58c8d48bc
raythurman2386 <raymondthurman5@gmail.com>
2019-09-13 19:23:37 -0400

    placeholder component

---
+++ team-builder/src/components/team/TeamPlaceholder.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TeamPlaceholder = () => {
+  return (
+    <div>
+      <h1>Oh dear! It appears you don't have a team!</h1>
+      <p>How about adding your team members?</p>
+    </div>
+  )
+}
+
+export default TeamPlaceholder
