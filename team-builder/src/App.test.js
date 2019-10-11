a4ba7a326575733502056e98d82bce24325481d3
raythurman2386 <raymondthurman5@gmail.com>
2019-10-10 22:22:32 -0400

    fixed tests to pass

--- team-builder/src/App.test.js
+++ team-builder/src/App.test.js
@@ -1,24 +1,31 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import * as rtl from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import App from './App'
+import Navbar from './components/navbar/Navbar'
 
 afterEach(rtl.cleanup)
 
-test('renders the content', () => {
-  const wrapper = rtl.render(<App />)
-  wrapper.debug()
-  // let addTeamMember = wrapper.getByText(/add team member/i)
-  // expect(addTeamMember).toBeInDocument()
+describe('My Test Suite', () => {
+  it('renders the app', () => {
+    const wrapper = rtl.render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    )
+    wrapper.debug()
+  })
+
+  it('renders the image', () => {
+    const wrapper = rtl.render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    )
+    // wrapper.debug()
+    let navLinks = wrapper.getAllByRole('img')
+    expect(navLinks.length).toBe(1)
+  })
 })
-
-// describe('My Test Suite', () => {
-//   it('renders the content', () => {
-//     const wrapper = rtl.render(<App />)
-
-//     let addTeamMember = wrapper.getByText(/add team member/i)
-
-//     expect(addTeamMember).toBeInDocument()
-//   })
-// })
